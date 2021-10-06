dac1a7e02440cc8447a5833840ebeb154385b678
Hasan Mohammad Rafi <91073073+rafi0215@users.noreply.github.com>
2021-10-06 21:24:47 +0600

    Rafi

--- src/masum/Account.java
+++ src/masum/Account.java
@@ -22,9 +22,8 @@ public class Account {
     }
 
     //set the pin number
-    public int setPinNumber(int pinNumber);
-    {
-        this.pinNumber=pinNumber;
+    public int setPinNumber(int pinNumber);{
+        this.pinNumber = pinNumber;
         return pinNumber;
     }
 
@@ -73,22 +72,71 @@ public class Account {
         savingBalance = (savingBalance + amount);
         return savingBalance;
     }
-    /*Customer Checking Account Withdraw Input*/
-    public  void getCheckingWithdrawInput(){
-        System.out.println("Checking Account Balance : "+moneyFormat.format(checkingBalance));
-        System.out.println("Ammount you want to withdraw from Checking Account :");
-        double amount =input.nextDouble();
-        if ((checkingBalance- amount)>=0){
-            calcCheckingWithdraw(amount);
-            System.out.println("New checking account balance:"+moneyFormat.format(checkingWithdraw));
 
-        }else{
-            System.out.println("Balance can not be negetive.");
+    /* Customer Checking  Amount Withdraw input */
+
+    public void getCheckingWithdrawInput() {
+        System.out.println("Checking Account Balance : " + moneyFormat.format.(checkingBalance));
+        System.out.println("Amount ypu want to withdraw from Checking Account: ");
+        double amount = input.nextDouble();
+
+        if ((checkingBalance - amount) >= 0){
+            calcCheckingWithdraw(amount);
+            System.out.println("New Checking Account balance: " + moneyFormat.format(check ));
+        } else {
+            System.out.println("Balance cannot be negative." + "\n");
         }
     }
-    /*Customer Saving Account Deposite Input*/
 
-    
+    /* Customer Saving Account Withdraw input */
 
+    public void getSavingWithdrawInput() {
+        System.out.println("Saving Account Balance: " +moneyFormat.format(savingBalance));
+        System.out.println("Amount you want to withdraw from Saving Account: ");
+        double amount = input.nextDouble();
 
+        if ((savingBalance - amount) >= 0) {
+            calcSavingWithdraw(amount);
+            System.out.println("New Saving Account balance: " + savingBalance + "\n");
+        } else {
+            System.out.println("Balance cannot be negative." + "\n");
+        }
+    }
+
+    /* Customer Checking Account Deposit input */
+
+public void getCheckingDepositInput() {
+    System.out.println("Checking Account Balance: " + moneyFormat.format(checkingBalance));
+    System.out.println("Amount you want to deposit from Checking Account: ");
+    double amount = input.nextDouble();
+
+    if ((checkingBalance + amount) >= 0) {
+        calcCheckingDeposit(amount);
+        System.out.println("New Checking Account balance: " + moneyFormat.format(check));
+    } else {
+        System.out.println("Balance cannot be negative." + "\n");
+    }
 }
+
+    /* Customer Saving  Account Deposit input */
+
+    public void getSavingDepositInput() {
+        System.out.println("Saving Account Balance: " + moneyFormat.format(savingBalance));
+        System.out.println("Amount you want to deposit from Saving Account: ");
+        double amount = input.nextDouble();
+
+        if ((savingBalance + amount) >= 0) {
+            calcSavingDeposit(amount);
+            System.out.println("New Saving Account balance: " + moneyFormat.format(savingBalance));
+        } else {
+            System.out.println("Balance cannot be negative." + "\n");
+        }
+    }
+
+    private  int customerNumber;
+    private  int pinNumber;
+    private double checkingBalance = 0;
+    private double savingBalance = 0;
+}
+
+
